2d16721497c652a601fdce2f2ff32c609f6a8919
FUTIKang <2760373022@qq.com>
2024-11-24 20:24:40 +0800

    站点更新

--- metadata/index.ts
+++ metadata/index.ts
@@ -2,7 +2,7 @@ import type { Creator } from '../scripts/types/metadata'
 import { getAvatarUrlByGithubName } from '../scripts/utils'
 
 /** 文本 */
-export const siteName = '孤南筏'
+export const siteName = '孤南筏的小屋'
 export const siteShortName = '孤南筏'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
@@ -19,17 +19,16 @@ export const plainTargetDomain = 'notebook.futikang.space'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
 
-/** 创作者 */
+/** 归属者 */
 export const creators: Creator[] = [
   {
     name: 'GU_Nanfa',
     avatar: '',
-        username: 'FUTIKang',
-    title: 'GNF-Social 原始创作者',
+    username: 'FUTIKang',
     desc: '全栈开发工程师, JAVA后端开发 & React前端开发',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/FUTIKang' },
-    //   { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+        { type: 'github', icon: 'github', link: 'https://github.com/FUTIKang' },
+        { type: 'gitee', icon: 'gitee', link: 'https://gitee.com/FUTIKang' }
     ],
     nameAliases: ['gunanfa', '孤南筏', '孤南筏', 'GU_Nanfa', 'GNF-Social'],
     emailAliases: ['2760373022@qq.com'],
